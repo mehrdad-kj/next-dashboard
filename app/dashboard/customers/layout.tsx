46eb1d7c3363feb1ddde79dee2b127f127215e44
mehrdad-kj <mehrdadk.jourabi@gmail.com>
2024-12-07 14:59:24 +0330

    Progress made

--- app/dashboard/customers/layout.tsx
+++ app/dashboard/customers/layout.tsx
@@ -1,6 +1,6 @@
 export default function Layout({ children }: { children: React.ReactNode }) {
   return (
-    <div className="flex-grow p-6 md:overflow-y-auto md:p-12 bg-yellow-500 text-green-400">
+    <div className="flex-grow p-6 md:overflow-y-auto md:p-12">
       {children}
     </div>
   );
